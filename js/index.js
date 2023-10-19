dc14fc02516e7e3bcae062823df53bab9994abf1
Vitor Domingos <vm486092@gmail.com>
2023-10-19 15:42:37 -0200

    adicionada a pasta js

---
+++ js/index.js
@@ -0,0 +1,37 @@
+
+// Definindo a variável profissaoFavorita com o valor "Programador web"
+var profissaoFavorita = "Programador web"
+
+// Verifica se a profissão favorita é "Programador web" e imprime uma mensagem correspondente
+if (profissaoFavorita === "Programador web") {
+    console.log("Minha profissão preferida é a de programador web.")
+} else if (profissaoFavorita === "Hacker") {
+    console.log("Minha profissão favorita é a de hacker.")
+} else if (profissaoFavorita === "Jogos Digitais") {
+    console.log("Minha profissão favorita é a de jogos digitais.")
+} else if (profissaoFavorita === "Desenvolvedor Mobile") {
+    console.log("Minha profissão favorita é a de desenvolvedor mobile.")
+} else if (profissaoFavorita === "Segurança da informação") {
+    console.log("Minha profissão favorita é de segurança da informação.")
+} else {
+    console.log("Minha profissão favorita não é nenhuma destas.")
+}
+
+// Definindo a variável gameFavorito com o valor "Fifa"
+var gameFavorito = "Fifa"
+
+// Verifica se o jogo favorito é "Fifa" e imprime uma mensagem correspondente
+if (gameFavorito === "Gears of War") {
+    console.log("Gears of War é o meu game favorito.")
+} else if (gameFavorito === "Batman Arkham City") {
+    console.log("Batman arkham city é o meu game favorito.")
+} else if (gameFavorito === "Forza Horizon") {
+    console.log("Forza Horizon é meu game favorito.")
+} else if (gameFavorito === "Street Fighter") {
+    console.log("Street Fighter é meu game favorito.")
+} else if (gameFavorito === "Halo Reach") {
+    console.log("Halo Reach é meu game favorito.")
+} else {
+    console.log("Meus jogos favoritos não são nenhum destes.")
+}
+
